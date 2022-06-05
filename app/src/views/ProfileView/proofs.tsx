715a69abfa1565c68ed96f799e40f7a9fb40c2ba
Bruce MacDonald <brucewmacdonald@gmail.com>
2022-06-05 13:34:25 -0400

    display info when user has not added any proofs

--- app/src/views/ProfileView/proofs.tsx
+++ app/src/views/ProfileView/proofs.tsx
@@ -31,5 +31,34 @@ export const Proofs: FC<{ identity: IdentityAlias }> = ({ identity }) => {
         </div>
       </div>
     )
-  else return <div>{proofsDisplay}</div>
+  else {
+    if (proofs.length == 0) {
+      return (
+        <div className="alert alert-info shadow-lg mt-2">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="stroke-current flex-shrink-0 w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+            <span className="ml-2">
+              This user has not added any proofs.
+              <div />
+              They must prefer to keep an air of mystery.
+            </span>
+          </div>
+        </div>
+      )
+    } else {
+      return <div>{proofsDisplay}</div>
+    }
+  }
 }
