082703ba14cc27ca29668af41b6a1342e0d8ab2d
Bruce MacDonald <brucewmacdonald@gmail.com>
2022-06-11 13:42:36 -0400

    normalize alias generation

--- app/src/utils/crypto.ts
+++ app/src/utils/crypto.ts
@@ -4,6 +4,10 @@ import * as anchor from '@project-serum/anchor'
 // generateAliasKeypair converts a string into a keypair that is used to store accounts on chain
 // this is useful to ensure uniqueness and (maybe) make look ups faster
 export const generateAliasKeypair = (alias: string): anchor.web3.Keypair => {
+  if (alias != process.env.NEXT_PUBLIC_ALIAS_FORMAT_EXCEPTION) {
+    alias = alias.toLowerCase()
+  }
+
   let hash: Uint8Array = createHash('sha256').update(alias).digest()
   return anchor.web3.Keypair.fromSeed(hash)
 }
